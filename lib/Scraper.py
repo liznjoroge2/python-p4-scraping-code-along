f3147daff605149e9bc2fd345e33ad29fc16c1f1
enjoroge <njrogeelizabeth53@gmail.com>
2024-09-04 21:27:49 +0300

    complete

--- lib/Scraper.py
+++ lib/Scraper.py
@@ -5,4 +5,13 @@ import ipdb
 
 
 class Scraper:
-    pass
+
+    def __init__(self):
+      self.courses = []
+
+    def get_page():
+        # more code coming soon!
+        doc =  BeautifulSoup(requests.get("http://learn-co-curriculum.github.io/site-for-scraping/courses").text, 'html.parser')
+        ipdb.set_trace()
+
+Scraper.get_page()
